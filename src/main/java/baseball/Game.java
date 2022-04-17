8a4542af8f0b3ea54db112076cd2de9d85bb6674
june2 <>
2022-04-17 22:45:01 +0900

    feat(Game) : Game 클래스 생성, play 세부 로직 작성 - computer, player 입력값 비교, 게임 승패여부 출력하기 - player 입력이 2일시, 게임 종료 - player 입력이 1일시, 새게임 시작

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,18 +1,51 @@
 package baseball;
 
+import baseball.model.Computer;
 import baseball.model.Player;
+import baseball.model.Rule;
+import baseball.view.ConsoleView;
+
+import java.util.Optional;
 
 public class Game {
     private boolean isRunning = true;
+    private Player player = new Player();
+    private Computer computer = new Computer();
+    private Rule rule = new Rule();
+    private ConsoleView view = new ConsoleView();
 
-    public void end() {
+    private void end() {
         this.isRunning = false;
+    }
+
+    private void run() {
+        view.play();
+        player.input();
+        this.match();
+    }
+
+    private void match() {
+        rule.reset();
+        rule.compare(computer.getValue(), player.getValue());
+        view.output(rule.getStrike(), rule.getBall());
+        if(rule.isAllStrike()) {
+            view.end();
+            view.replay();
+            this.checkReplay();
+        }
+    }
+
+    private void checkReplay() {
+        player.input();
+        if (player.getValue().equals("2")) {
+            this.end();
+        }
+        computer.setValue(3);
     }
 
     public void play() {
         do {
-            System.out.println("play");
-            this.end();
+            this.run();
         } while (this.isRunning);
     }
 }
