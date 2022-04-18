47f4ebd1d97d314ec2e44b16214d6814fbb7ec27
june2 <>
2022-04-18 11:41:25 +0900

    feat(Config) : replay 상수값 추가 feat(NumberUtil) : numeric 체크 함수 추가

--- src/main/java/baseball/constant/Config.java
+++ src/main/java/baseball/constant/Config.java
@@ -2,5 +2,6 @@ package baseball.constant;
 
 public class Config {
     public static final int NUMBER_LENGTH = 3;
+    public static final String REPLAY_KEY = "1";
     public static final String END_KEY = "2";
 }
